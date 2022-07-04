a914bcd0f04dee63cb0ec0667b69bcba36d1ecdb
Appeltje#5053 <93653477+APPELTJE-SCRIPTS@users.noreply.github.com>
2022-07-04 12:25:39 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 RegisterCommand("me", function(source , args, rawCommand)
     local text = table.concat(args, ' ')
     text = string.sub(text,1,Config.MaxLength)
-    TriggerClientEvent('sn:3dme', -1, text, source)
+    TriggerClientEvent('ap:3dme', -1, text, source)
 end, false)
